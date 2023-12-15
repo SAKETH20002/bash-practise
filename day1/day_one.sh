b4da6c6c64af5a1a501ddf0b408014b19c4beecf
SAKETH20002 <sakethmutyala2625@gmail.com>
2023-12-15 11:57:33 +0000

    final stage of day1

--- day1/day_one.sh
+++ day1/day_one.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
+ set -x
+
 #"#" is used for comment.
 
 echo "HELLO STRANGER "
- 
+
+#declaring variables
+
+a=12
+b=10
+expr $a + $b
+
+ #"$_" the last argument of the previous command.
+ echo  $_
+#"$*" All the arguments on the command line.
+echo $*
+
+# commandline arrguments.
+echo "give a name"
+
+sleep 3s
+
+echo  $1 "is a nice name "
+
+#"$#" the number of command-line arguments.
+echo $#
+
+# wild card 
+# "*" is a wild card which help to search files .
+ls f*
